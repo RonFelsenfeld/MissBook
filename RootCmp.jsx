0739f5a0990ecee3c24a1f1601c2b69781ec6108
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-03-10 14:41:10 +0200

    Init Repo

---
+++ RootCmp.jsx
@@ -0,0 +1,12 @@
+import { Home } from './pages/Home.jsx'
+
+export function App() {
+  return (
+    <section className="app">
+      <header className="app-header">
+        <h1>Miss Book</h1>
+      </header>
+      <main className="container"></main>
+    </section>
+  )
+}
